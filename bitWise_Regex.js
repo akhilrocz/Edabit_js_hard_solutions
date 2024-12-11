2ffa4491da0cff72f5d57235c9b10695aabbb49a
Akhil roczz <b.akhil667@gmail.com>
2024-12-11 19:58:56 +0530

    bitwise_regex

---
+++ bitWise_Regex.js
@@ -0,0 +1,12 @@
+function isOdd(n) {
+  return (n & 1) === 1 ? "Yes" : "No";
+}
+function isEven(str) {
+  const regex = /^-?\d*[02468]$/;
+  return regex.test(str) ? "Yes" : "No";
+}
+console.log(isOdd(3));
+console.log(isOdd(58));
+
+console.log(isEven("0"));
+console.log(isEven("-99"));
