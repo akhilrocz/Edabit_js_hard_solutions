586f47b7c6c235205fba6cd40e3d310cf0558f37
Akhil roczz <b.akhil667@gmail.com>
2024-12-17 22:36:45 +0530

    Disarium Number

---
+++ disariumNumber.js
@@ -0,0 +1,19 @@
+function isDisarium(num) {
+  let numInString = num.toString();
+  let sum = 0;
+  for (let i = 0; i < numInString.length; i++) {
+    sum += numInString[i] ** (i + 1);
+  }
+  if (sum === num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isDisarium(75));
+console.log(isDisarium(135));
+console.log(isDisarium(544));
+console.log(isDisarium(518));
+console.log(isDisarium(8));
+console.log(isDisarium(466));
