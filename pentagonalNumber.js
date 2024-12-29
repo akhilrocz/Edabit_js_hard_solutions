393885b44f5bcd48c71bcca97d07ce505462b521
Akhil roczz <b.akhil667@gmail.com>
2024-12-29 23:29:20 +0530

    Pentagonal Number

---
+++ pentagonalNumber.js
@@ -0,0 +1,8 @@
+function pentagonal(num) {
+  return (5 * (num * num) - 5 * num + 2) / 2;
+}
+
+console.log(pentagonal(1));
+console.log(pentagonal(2));
+console.log(pentagonal(3));
+console.log(pentagonal(8));
