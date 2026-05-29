92384292a85ebf0014d13a8cb7023aec83fed7f2
Akhil roczz <b.akhil667@gmail.com>
2026-05-29 20:38:47 +0530

    added changes

---
+++ functionTimesThree.js
@@ -0,0 +1,8 @@
+function product([a, b], [c, d], [e, f]) {
+  console.log(a * c * e + b * d * f);
+}
+
+product([1, 2], [1, 1], [2, 3]);
+product([10, 2], [5, 0], [2, 3]);
+product([1, 2], [2, 3], [3, 4]);
+product([1, 2], [0, 3], [3, 0]);
